d17a4a73428488cf6234933783f48d19a0290ea3
𝕄ℝ. 𝕂𝔸𝕋ℍ𝕀ℝ𝕀𝕐𝔸 👁️⃤ <kathiriyarahil143@gmail.com>
2024-07-26 18:23:55 +0530

    Add files via upload

---
+++ pr/3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c;
+	printf("enter A = ");
+	scanf("%d",&a); 
+	
+	printf("enter B = ");
+	scanf("%d",&b); 
+	
+	c=a+b;
+	
+    printf("Third angle: %d",c-180);
+}
